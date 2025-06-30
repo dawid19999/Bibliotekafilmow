d1e6de986f9c758dfaa99cb65254a032e18903e9
Dawid <dawid.petkowicz123wp.pl@wp.pl>
2025-06-30 21:48:45 +0200

    Update Bibliotekafilmow.py

--- Bibliotekafilmow.py
+++ Bibliotekafilmow.py
@@ -2,9 +2,7 @@
 import random
 
 
-class Movie:
-    content_type = "movie"  
-
+class Video:
     def __init__(self, title, year, genre):
         self.title = title
         self.year = year
@@ -14,13 +12,11 @@ class Movie:
     def play(self):
         self.plays += 1
 
+class Movie(Video):
     def __str__(self):
         return f"{self.title} ({self.year})"
 
-
-class Series(Movie):
-    content_type = "series"  
-
+class Series(Video):
     def __init__(self, title, year, genre, season, episode):
         super().__init__(title, year, genre)
         self.season = season
@@ -29,22 +25,16 @@ class Series(Movie):
     def __str__(self):
         return f"{self.title} S{self.season:02}E{self.episode:02}"
 
-
-def filter_and_sort(library, content_type):
-    return sorted([item for item in library if item.content_type == content_type], key=lambda x: x.title)
-
-
 def get_movies(library):
-    return filter_and_sort(library, "movie")
-
+    movies = [item for item in library if type(item) == Movie]
+    return sorted(movies, key=lambda x: x.title)
 
 def get_series(library):
-    return filter_and_sort(library, "series")
-
+    series = [item for item in library if type(item) == Series]
+    return sorted(series, key=lambda x: x.title)
 
 def search(library, title):
     return [item for item in library if title.lower() in item.title.lower()]
-
 
 def generate_views(library):
     item = random.choice(library)
@@ -56,7 +46,6 @@ def generate_views(library):
 def run_generate_views(library, times=10):
     for _ in range(times):
         generate_views(library)
-
 
 def top_titles(library, n=3, content_type=None):
     if content_type == "movie":
